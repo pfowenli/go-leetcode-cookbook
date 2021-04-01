583a8bf029d2c776be03c3d4d6bffa8d52e3aa0f
Owen P. F. Li <pfowenli@gmail.com>
2021-04-01 18:10:10 +0800

    upload 0344.solution.go

---
+++ leetcode/0344.reverse-string/0344.solution.go
@@ -0,0 +1,13 @@
+package leetcode
+
+func reverseString(s []byte) []byte {
+    i, j := 0, len(s) - 1
+    
+    for i < j {
+        s[i], s[j] = s[j], s[i]
+        i++
+        j--
+    }
+    
+    return s
+}
