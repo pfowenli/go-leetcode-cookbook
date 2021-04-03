cb60692423e5777f6e20f4f608f79abdf0c793a6
Owen P. F. Li <pfowenli@gmail.com>
2021-04-03 23:30:51 +0800

    upload 0136.solution.go

---
+++ leetcode/0136.single-number/0136.solution.go
@@ -0,0 +1,13 @@
+package leetcode
+
+func singleNumber(nums []int) int {
+    result := 0
+
+    for i := 0; i < len(nums); i++ {
+        // XOR operator
+        result ^= nums[i]
+    }
+
+    return result
+}
+
