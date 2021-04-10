5b613bf4137c51b7a33ddf630bd8a496f4e6ab08
Owen P. F. Li <pfowenli@gmail.com>
2021-04-10 23:41:47 +0800

    upload 0217.solution.go

---
+++ leetcode/0217.contains-duplicate/0217.solution.go
@@ -0,0 +1,15 @@
+package leetcode
+
+func containsDuplicate(nums []int) bool {
+    numMap := make(map[int]bool)
+    
+    for _, num := range nums {
+        if numMap[num] {
+            return true
+        }
+        numMap[num] = true
+    }
+    
+    return false
+}
+
