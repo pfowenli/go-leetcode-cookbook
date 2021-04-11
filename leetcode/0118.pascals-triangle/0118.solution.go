8bc103aace96a23b909c17978e74c4b8f3b2558d
Owen P. F. Li <pfowenli@gmail.com>
2021-04-11 23:16:22 +0800

    upload 0118.solution.go

---
+++ leetcode/0118.pascals-triangle/0118.solution.go
@@ -0,0 +1,20 @@
+package leetcode
+
+func generate(numRows int) [][]int {
+    var results [][]int
+    
+    results = append(results, []int{1})
+
+    for i := 1; i < numRows; i++ {
+        row := make([]int, i + 1)
+        results = append(results, row)
+
+        for j := 0; j < i; j++ {
+            results[i][j] += results[i-1][j]
+            results[i][j+1] += results[i-1][j]
+        }
+    }
+    
+    return results
+}
+
