28bc72a407304886cec14aca17c92d37f46ee3c8
Owen P. F. Li <pfowenli@gmail.com>
2021-04-12 22:57:25 +0800

    upload 0268.solution.go

---
+++ leetcode/0268.missing-number/0268.solution.go
@@ -0,0 +1,12 @@
+package leetcode
+
+func missingNumber(nums []int) int {
+    length := len(nums)
+    sum := (1 + length) * length / 2
+    
+    for _, value := range nums {
+        sum -= value
+    }
+
+    return sum
+}
