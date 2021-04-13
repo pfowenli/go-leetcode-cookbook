b96be7ae6d5f2204abbdf969a3a8ba27d29428ee
Owen P. F. Li <pfowenli@gmail.com>
2021-04-13 21:32:14 +0800

    upload 0191.solution.go

---
+++ leetcode/0191.number-of-1-bits/0191.solution.go
@@ -0,0 +1,16 @@
+package leetcode
+
+func hammingWeight(num uint32) int {
+    onesCount := 0
+
+    for num != 0 {
+        if num & 1 == 1 {
+            onesCount++
+        }
+
+        num = num>>1
+    }
+    
+    return onesCount    
+}
+
