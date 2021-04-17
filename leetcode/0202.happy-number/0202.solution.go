f373ad731dc37121efa708decc71585713477a70
Owen P. F. Li <pfowenli@gmail.com>
2021-04-17 18:54:01 +0800

    upload 0202.solution.go

---
+++ leetcode/0202.happy-number/0202.solution.go
@@ -0,0 +1,29 @@
+package leetcode
+
+func isHappy(n int) bool {
+    valueMap := make(map[int]bool)
+    
+    for !valueMap[n] {
+        valueMap[n] = true
+        n = convertNumber(n)
+
+        if n == 1 {
+            return true
+        }
+    }
+    
+    return false
+}
+
+func convertNumber(num int) int {
+    sum := 0
+    
+    for num > 0 {
+        remain := num % 10
+        sum += remain * remain
+        num = num / 10
+    }
+    
+    return sum
+}
+
