a762e02a2e1503a9e89105da5b46a76f750f7ca2
Owen P. F. Li <pfowenli@gmail.com>
2021-04-20 23:54:34 +0800

    upload 0190.solution.go

---
+++ leetcode/0190.reverse-bits/0190.solution.go
@@ -0,0 +1,15 @@
+package leetcode
+
+func reverseBits(num uint32) uint32 {
+    result := uint32(0)
+    power := uint32(31)
+
+    for num != 0 {
+        result += (num & 1) << power
+        num = num >> 1
+        power -= 1        
+    }
+    
+    return result
+}
+
