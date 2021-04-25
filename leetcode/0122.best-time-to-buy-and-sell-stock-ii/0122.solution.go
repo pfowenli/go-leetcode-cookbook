4a858998feff1f561c7d6bad7857c44890fcc8dc
Owen P. F. Li <pfowenli@gmail.com>
2021-04-25 22:39:53 +0800

    upload 0122.solution.gp

---
+++ leetcode/0122.best-time-to-buy-and-sell-stock-ii/0122.solution.go
@@ -0,0 +1,18 @@
+package leetcode
+
+func maxProfit(prices []int) int {
+    profit := int(0)
+
+    for index, price := range prices {
+        if index == 0 {
+            continue
+        }
+
+        if price > prices[index - 1] {
+            profit += price - prices[index - 1]
+        }
+    }
+
+    return profit
+}
+
