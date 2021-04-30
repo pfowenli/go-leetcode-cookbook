c7cdacdaffeb3790006c03715f682c46531ec851
Owen P. F. Li <pfowenli@gmail.com>
2021-04-30 23:14:44 +0800

    upload 0028.implement-strstr

---
+++ leetcode/0028.implement-strstr/0028.solution.go
@@ -0,0 +1,31 @@
+package leetcode
+
+func strStr(haystack string, needle string) int {
+    if len(needle) == 0 {
+        return 0
+    }
+
+    if len(needle) > len(haystack) {
+        return -1
+    }
+
+    for index := 0; index <= len(haystack) - len(needle); index++ {
+        if isSameString(haystack[index: index + len(needle)], needle) {
+            return index
+        }
+    }
+    return -1
+}
+
+func isSameString(a string, b string) bool {
+    if len(a) != len(b) {
+        return false
+    }
+
+    for index, char := range a {
+        if string(char) != b[index:index+1] {
+            return false
+        }
+    }
+    return true
+}
---
+++ leetcode/0028.implement-strstr/0028.solution_test.go
@@ -0,0 +1,32 @@
+package leetcode
+
+import (
+    "testing"
+)
+
+func TestStrStrI(t *testing.T) {
+    haystack := "hello"
+    needle := "ll"
+    expected := 2
+    if got := strStr(haystack, needle) ; got != expected {
+        t.Errorf("strStr() = %d, expected %d", got, expected)
+    } 
+}
+
+func TestStrStrII(t *testing.T) {
+    haystack := "aaaaa"
+    needle := "bba"
+    expected := -1
+    if got := strStr(haystack, needle) ; got != expected {
+        t.Errorf("strStr() = %d, expected %d", got, expected)
+    } 
+}
+
+func TestStrStrIII(t *testing.T) {
+    haystack := ""
+    needle := ""
+    expected := 0
+    if got := strStr(haystack, needle) ; got != expected {
+        t.Errorf("strStr() = %d, expected %d", got, expected)
+    } 
+}
