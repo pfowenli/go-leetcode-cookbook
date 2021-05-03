5b0e19e244bedc3c32bc0fa93d77e29f484112d0
Owen P. F. Li <pfowenli@gmail.com>
2021-05-03 23:51:14 +0800

    upload 0287.solution.go

---
+++ leetcode/0287.find-the-duplicate-number/0287.solution.go
@@ -0,0 +1,17 @@
+package leetcode
+
+func findDuplicate(nums []int) int {
+    result := int(0)
+    numSet := make(map[int]bool)
+    
+    for _, num := range nums {
+        if numSet[num] {
+            result = num
+            break
+        }
+        numSet[num] = true
+    }
+
+    return result
+}
+
