6b0526ed9e409c50bef6c6779b73d319ac899d6f
Owen P. F. Li <pfowenli@gmail.com>
2021-05-04 00:11:44 +0800

    upload 0287.solution_test.go

---
+++ leetcode/0287.find-the-duplicate-number/0287.solution_test.go
@@ -0,0 +1,42 @@
+package leetcode
+
+import (
+    "testing"
+)
+
+func TestFindDuplicateI(t *testing.T) {
+    nums := []int{1,3,4,2,2}
+    expected := int(2)
+
+    if got := findDuplicate(nums); got != expected {
+        t.Errorf("findDuplicate() = %d, expected = %d", got, expected)
+    }
+}
+
+func TestFindDuplicateII(t *testing.T) {
+    nums := []int{3,1,3,4,2}
+    expected := int(3)
+
+    if got := findDuplicate(nums); got != expected {
+        t.Errorf("findDuplicate() = %d, expected = %d", got, expected)
+    }
+}
+
+func TestFindDuplicateIII(t *testing.T) {
+    nums := []int{1,1}
+    expected := int(1)
+
+    if got := findDuplicate(nums); got != expected {
+        t.Errorf("findDuplicate() = %d, expected = %d", got, expected)
+    }
+}
+
+func TestFindDuplicateIV(t *testing.T) {
+    nums := []int{1,1,2}
+    expected := int(1)
+
+    if got := findDuplicate(nums); got != expected {
+        t.Errorf("findDuplicate() = %d, expected = %d", got, expected)
+    }
+}
+
