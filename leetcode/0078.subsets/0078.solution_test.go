eac689eb063a640bc43bfd5b631b18808f88bdd5
Owen P. F. Li <pfowenli@gmail.com>
2021-05-07 23:29:48 +0800

    undone 0078.solution_test.go

--- leetcode/0078.subsets/0078.solution_test.go
+++ leetcode/0078.subsets/0078.solution_test.go
@@ -1,8 +1,8 @@
 package leetcode
 
 import (
+	"reflect"
 	"testing"
-        "reflect"
 )
 
 func TestSubsetsI(t *testing.T) {
@@ -17,15 +17,6 @@ func TestSubsetsI(t *testing.T) {
 func TestSubsetsII(t *testing.T) {
 	nums := []int{1}
 	expected := [][]int{[]int{}, []int{1}}
-
-	if got := subsets(nums); !reflect.DeepEqual(got, expected) {
-		t.Errorf("subsets() = %d, expected = %d", got, expected)
-	}
-}
-
-func TestSubsetsIII(t *testing.T) {
-	nums := []int{1, 2, 3, 4, 5, 6}
-	expected := [][]int{[]int{}, []int{1}, []int{1, 2}, []int{1, 2, 3}, []int{2}, []int{2, 3}, []int{3}}
 
 	if got := subsets(nums); !reflect.DeepEqual(got, expected) {
 		t.Errorf("subsets() = %d, expected = %d", got, expected)
