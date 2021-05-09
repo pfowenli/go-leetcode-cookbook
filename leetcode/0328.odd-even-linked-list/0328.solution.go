2ac43328de67bba2c09060ca60dd0419c8e1598b
Owen P. F. Li <pfowenli@gmail.com>
2021-05-10 01:46:40 +0800

    reupload 0328.solution.go

--- leetcode/0328.odd-even-linked-list/0328.solution.go
+++ leetcode/0328.odd-even-linked-list/0328.solution.go
@@ -8,5 +8,38 @@ package leetcode
  * }
  */
 func oddEvenList(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil || head.Next.Next == nil {
+		return head
+	}
 
+	node := head.Next.Next
+	oddTail := head
+	evenHead := head.Next
+	evenTail := head.Next
+
+	oddTail.Next = nil
+	evenHead.Next = nil
+
+	oddOrNot := false
+	
+	for node != nil {
+		temp := node
+		node = node.Next
+		temp.Next = nil
+
+		oddOrNot = !oddOrNot
+
+		if oddOrNot {
+			oddTail.Next = temp
+			oddTail = temp
+			continue
+		}
+		
+		evenTail.Next = temp
+		evenTail = temp
+	}
+
+	oddTail.Next = evenHead
+
+	return head
 }
