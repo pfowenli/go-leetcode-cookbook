ce1606fc4aa6cd81b8beebf252f18411a48b9661
Owen P. F. Li <pfowenli@gmail.com>
2021-05-11 22:05:58 +0800

    upload 0062.solution.go

--- leetcode/0062.unique-paths/0062.solution.go
+++ leetcode/0062.unique-paths/0062.solution.go
@@ -1,9 +1,16 @@
 package leetcode
 
 func uniquePaths(m int, n int) int {
-	if m == 1 || n == 1 {
-		return 1
+	pathCounts := make([]int, n)
+	for i := range pathCounts {
+		pathCounts[i] = 1
 	}
 
-	return uniquePaths(m - 1, n) + uniquePaths(m, n - 1)
+	for i := 1; i < m; i++ {
+		for j := 1; j < len(pathCounts); j++ {
+			pathCounts[j] += pathCounts[j-1]
+		}
+	}
+
+	return pathCounts[n-1]
 }
