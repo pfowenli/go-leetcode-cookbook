335998cab8c6d23651270d4157da8ae2483a707a
Owen P. F. Li <pfowenli@gmail.com>
2021-06-24 23:55:14 +0800

    reupload 0234.palindrome-linked-list

--- leetcode/0234.palindrome-linked-list/solution.go
+++ leetcode/0234.palindrome-linked-list/solution.go
@@ -1,6 +1,30 @@
 package leetcode
 
-type ListNode struct {
-	Val  int
-	Next *ListNode
+func isPalindrome(head *ListNode) bool {
+	slow := head
+	fast := head
+	var rev *ListNode = nil
+
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+
+		previous := rev
+		rev = slow
+		slow = slow.Next
+		rev.Next = previous
+	}
+
+	if fast != nil {
+		slow = slow.Next
+	}
+
+	for rev != nil && slow != nil {
+		if slow.Val != rev.Val {
+			return false
+		}
+		slow = slow.Next
+		rev = rev.Next
+	}
+
+	return true
 }
