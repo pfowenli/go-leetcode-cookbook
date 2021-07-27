30ea4c3108e261dc2950e71e5a9d5b3a3f891f2a
Owen P. F. Li <pfowenli@gmail.com>
2021-07-27 23:51:36 +0800

    0112.path-sum

---
+++ leetcode/0112.path-sum/solution.go
@@ -0,0 +1,23 @@
+package leetcode
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func hasPathSum(root *TreeNode, targetSum int) bool {
+	if root == nil {
+		return false
+	}
+
+	targetSum -= root.Val
+
+	if root.Left == nil && root.Right == nil && targetSum == 0 {
+		return true
+	}
+
+	return hasPathSum(root.Left, targetSum) || hasPathSum(root.Right, targetSum)
+}
