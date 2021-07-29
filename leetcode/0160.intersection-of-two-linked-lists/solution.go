039cb3a751201dc26aad796e510b0695f5399bc1
Owen P. F. Li <pfowenli@gmail.com>
2021-07-29 23:56:30 +0800

    reupload 0160.intersection-of-two-linked-lists

---
+++ leetcode/0160.intersection-of-two-linked-lists/solution.go
@@ -0,0 +1,29 @@
+package leetcode
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	nodeA := headA
+	nodeB := headB
+
+	for nodeA != nodeB {
+		if nodeA == nil {
+			nodeA = headB
+		} else {
+			nodeA = nodeA.Next
+		}
+
+		if nodeB == nil {
+			nodeB = headA
+		} else {
+			nodeB = nodeB.Next
+		}
+	}
+
+	return nodeA
+}
