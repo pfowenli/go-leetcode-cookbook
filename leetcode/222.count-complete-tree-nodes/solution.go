7062606f591dbdae75f8e6e3c40b94396d4f8688
Owen P. F. Li <pfowenli@gmail.com>
2021-07-31 00:05:27 +0800

    upload 222.count-complete-tree-nodes

---
+++ leetcode/222.count-complete-tree-nodes/solution.go
@@ -0,0 +1,40 @@
+package leetcode
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+import (
+    "math"
+)
+
+func countNodes(root *TreeNode) int {
+    if root == nil {
+        return 0
+    }
+    
+    leftNode, rightNode := root.Left, root.Right
+    leftHeight, rightHeight := 0, 0
+    
+    for leftNode != nil {
+        leftHeight++
+        leftNode = leftNode.Left
+    }
+    
+    for rightNode != nil {
+        rightHeight++
+        rightNode = rightNode.Right
+    }
+    
+    if leftHeight == rightHeight {
+        return int(math.Pow(2, float64(leftHeight + 1)) - 1)
+    }
+    
+    return countNodes(root.Left) + countNodes(root.Right) + 1
+}
+
