31f92e187960807a36f04c7e493b75d0e82d596d
Owen P. F. Li <pfowenli@gmail.com>
2021-08-15 23:52:07 +0800

    revise 0496.next-greater-element-i

--- leetcode/0496.next-greater-element-i/solution.go
+++ leetcode/0496.next-greater-element-i/solution.go
@@ -2,25 +2,21 @@ package leetcode
 
 func nextGreaterElement(nums1 []int, nums2 []int) []int {
 	numMap := make(map[int]int)
+	stack := []int{}
 
-	for idx, num2 := range nums2 {
-		numMap[num2] = idx
-	}
-
-	results := []int{}
-
-	for _, num1 := range nums1 {
-		result := -1
-
-		for j := numMap[num1] + 1; j < len(nums2); j++ {
-			if num1 < nums2[j] {
-				result = nums2[j]
-				break
-			}
+	for _, num := range nums2 {
+		for len(stack) > 0 && stack[len(stack)-1] < num {
+			numMap[stack[len(stack)-1]] = num
+			stack = stack[:len(stack)-1]
 		}
 
-		results = append(results, result)
+		stack = append(stack, num)
+		numMap[num] = -1
 	}
 
-	return results
+	for idx, num := range nums1 {
+		nums1[idx] = numMap[num]
+	}
+
+	return nums1
 }
