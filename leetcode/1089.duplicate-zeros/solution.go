dbdb59b14fd04a965ff78ab4ec4a3e4e642a1a2a
Owen P. F. Li <pfowenli@gmail.com>
2021-08-24 15:12:29 +0800

    upload 1089.duplicate-zeros

---
+++ leetcode/1089.duplicate-zeros/solution.go
@@ -0,0 +1,33 @@
+package leetcode
+
+func duplicateZeros(arr []int)  {
+    left := 0
+    right := len(arr) - 1
+    
+    for left < right {
+        if arr[left] == 0 {
+            right--
+        }
+        left++
+    }
+    
+    if right == left {
+        arr[len(arr) - 1] = arr[left]
+        left--
+        right = len(arr) - 2
+    } else {
+        left = right
+        right = len(arr) - 1
+    }
+
+    for left < right {
+        if arr[left] == 0 {
+            arr[right] = 0
+            right--
+        }
+        
+        arr[right] = arr[left]
+        right--
+        left--
+    }
+}
